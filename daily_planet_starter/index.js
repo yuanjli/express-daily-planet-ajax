e34fe04f814846bcfb412b3ce56a107d900c0767
yuanjli <liyuanjienetwork@gmail.com>
2018-08-20 17:01:01 -0700

    fix the edit form button

--- daily_planet_starter/index.js
+++ daily_planet_starter/index.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+
 // The back end JavaScript code;
 
 // express web framework;
 // Requires:
-=======
+
 // This is the back end JavaScript file;
 
 //the express web framework. 
 // an example of ======python is Django=======; 
 
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
+
 var express = require('express');
 //  --> This is what allows us to use the body partial in layout.
 
@@ -18,14 +18,8 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-<<<<<<< HEAD
-// 
-=======
-
 // ----> MIDDILEWARE <-------
 
-
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
 app.use(partials());
 
 // EJS is an template engine for EJS;
@@ -50,7 +44,6 @@ app.get('/', function(req, res) {
 });
 
 app.get('/articles', function(req, res) {
-
     res.render('articles/index', { articles: articles });
 });
 
@@ -61,7 +54,7 @@ app.get('/articles/new', function(req, res) {
 app.get('/articles/:index', function(req, res) {
     var index = parseInt(req.params.index);
     if (index < articles.length && index >= 0) {
-        res.render('articles/show', { article: articles[req.params.index], index: req.params.index });
+        res.render('articles/show', { article: articles[req.params.index], index: req.params.index});
     } else {
         res.send('Error');
     }
@@ -76,38 +69,23 @@ app.get('/about', function(req, res) {
     res.render('about');
 });
 
-<<<<<<< HEAD
-// function for edit 
-app.get('/articles/: index/edit', function(req, res){
-    var index = parseInt(req.params.index);
-    // Checks if the request exist;
-    if (index<articles.length && index>=0) {  
-        res.render('articles/edit', {articles: articles[index], index: index}); 
-=======
 
-app.get('/articles/: index/edit', function(req, res){
+// function for edit 
+app.get('/articles/:index/edit', function(req, res){
     var index = parseInt(req.params.index);
     //res.send(req.params.index);  // this checks the req.params.index is sending. 
     if (index<articles.length && index >=0) {
-        res.render('articles/edit', {articles: articles[index], index: index});
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
+        console.log("THIS IS THE INDEX:", req.params.index);
+        res.render('articles/edit', {article: articles[index], index: req.params.index});
     } else {
         res.send('Error');
     }
 });
 
-<<<<<<< HEAD
-// The delete function and works for the delete button;
-app.delete('/articles/:index', function(req, res){
-    var index = parseInt(req.params.index);
-    if (index<articles.length && index>=0){
-=======
-
 // The Delete function and works for the delete button;
 app.delete('/articles/:index', function(req, res){
     var index = parseInt(req.params.index);
     if (index < articles.length && index>=0) {
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
         articles.splice(index, 1);
         res.send('success');
     } else {
@@ -115,49 +93,18 @@ app.delete('/articles/:index', function(req, res){
     }
 });
 
-<<<<<<< HEAD
 // The put function for changing the body;
 app.put("/articles/:index", function(req, res){
     var index = parseInt(req.params.index);
-    console.log(req.body);
+    console.log("this is the body of the req:", req.body);
     if (index<articles.length && index>=0) {
         articles[req.params.index] = req.body;
         res.send("success!");
     } else {
         res.send("Error!");
-=======
-app.put("/articles/:index", function(req, res){
-    var index = parseInt(req.params.index);
-    console.log(req.body);
-    if (index < articles.length && index>=0) {
-        articles[req.params.index] = req.body;
-        res.send("success!");
-    } else {
-        res.send("error!");
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
     }
 });
 
 app.listen(3000, function() {
     console.log("You're listening to the smooth sounds of port 3000 in the morning");
 });
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
-
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
--- daily_planet_starter/static/ajax-routing.js
+++ daily_planet_starter/static/ajax-routing.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-// The front end JavaScript;
 
-$(document).ready(function(){
-	// Get the detele button to work on the ajax;
-	$("#delete-btn").on("click", function(e){
-		e.preventDefault();
-
-		var url = $(this).attr("href");
-=======
 // This is the front end JavaScript;
 
 $(document).ready(function(){
@@ -16,62 +7,36 @@ $(document).ready(function(){
 		e.preventDefault(); // stops the default click event response, and prevent the page from refreshing.
 
 		var url = $(this).attr("href"); // Get href from delete link button.
-
-		//console.log("Delete btn clicked!");
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
+		console.log(url);
+		console.log("Delete btn clicked!");
 
 		$.ajax({
 			method: "DELETE",
 			url: url
 		}).done(function(data){
-			window.location = "/articles";
+			//window.location = "/articles";
 		}).fail(function(err){
 			console.log("Error:", err);
-<<<<<<< HEAD
 		});
 	});	
 
 	// Get the edit button to work on the ajax;
 	$("#edit-form").on("submit", function(e){
-		e.preventDefault();
-=======
-		}); // End of AJAX DELETE 
-	});
-
-
-	$("#edit-form").on("submit", function(e){
 		e.preventDefault(); // stops the submit default functionality;
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
 
 		var url = $(this).attr("action");
 		var data = $(this).serialize();
-		console.log(data);
+		console.log(url);
 
 		$.ajax({
 			method: "PUT",
 			url: url,
 			data: data
 		}).done(function(data){
-			window.location = "/articles";
+			window.location = url;
 		}).fail(function(err){
 			console.log("Error:", err);
-<<<<<<< HEAD
+
 		});
 	});
-});
-=======
-		}); // End AJAX PUT
-	});
-});
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> d82dbe1464dbc3402ea971eb751c3c80020c93c0
+}); // End AJAX PUT
